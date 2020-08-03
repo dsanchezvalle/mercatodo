bfa8a49e61460014f1d48b3a6cbcfac950f6ee4d
David <dsanva2004@gmail.com>
2020-08-02 19:13:49 -0500

    refactor: move filter partial view to component

---
+++ app/Http/Controllers/Client/BookController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BookFilterRequest;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function bookshelf(BookFilterRequest $request)
+    {
+
+    }
+}
