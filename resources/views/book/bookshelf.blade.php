c7d4c025e47381a48ce647b68f9f7ee75c5860a5
David <dsanva2004@gmail.com>
2020-08-04 11:15:46 -0500

    fix: minor fix in filter content display.

--- resources/views/book/bookshelf.blade.php
+++ resources/views/book/bookshelf.blade.php
@@ -10,27 +10,32 @@
                     <x-book-filter route="{{ route('bookshelf') }}"></x-book-filter>
                 </div>
             </div>
-            <hr>
+
         </div>
         <div class="m-2 row justify-content-center align-content-center">
             <h2>Find the perfect Book for you!</h2>
         </div>
 
-        <div class="card-group row row-cols-lg-5 row-cols-2">
+        @if(count($books)>0)
+            <div class="card-group row row-cols-lg-5 row-cols-2">
             @foreach($books as $book)
-                <div class="col mb-4 d-flex">
-                    <div class="card">
-                        <img class="card-img-top" src="{{$book->image_path}}" alt="book-cover">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $book->title }}</h5>
-                            <p class="card-subtitle mb-2 text-muted">Author: {{ $book->author }}</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Price: {{ $book->formattedPrice($book->price) }}</small>
+                    <div class="col mb-4 d-flex">
+                        <div class="card">
+                            <img class="card-img-top" src="{{$book->image_path}}" alt="book-cover">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $book->title }}</h5>
+                                <p class="card-subtitle mb-2 text-muted">Author: {{ $book->author }}</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Price: {{ $book->formattedPrice($book->price) }}</small>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+        @else
+            <p class="alert-info" colspan="5" style="font-size:110%; text-align: center">There are no books to show. Try another filter.</p>
+        @endif
+
         </div>
         <div class="m-2 row justify-content-center align-content-center">
             {{ $books->withQueryString()->links() }}
