f68c0de8338d17401a7b40630cc278bc4d2bfe96
David <dsanva2004@gmail.com>
2020-09-27 16:23:06 -0500

    feat: creation of Transaction model and create_transactions_table migration.

---
+++ database/migrations/2020_09_27_205541_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders');
+
+            $table->string('reference', 32);
+            $table->float('amount', 10, 2, true);
+            $table->unsignedInteger('request_id')->nullable();
+            $table->string('status', 32);
+            $table->string('process_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
