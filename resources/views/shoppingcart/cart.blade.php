a43b559620e1b7790274b29e186daa9948c1981e
David <dsanva2004@gmail.com>
2020-09-28 17:43:59 -0500

    fix: change function getSubtotal for getFormattedSubtotal and add a button to get more books when the shopping cart is empty.

--- resources/views/shoppingcart/cart.blade.php
+++ resources/views/shoppingcart/cart.blade.php
@@ -41,7 +41,7 @@
                             @endforeach
                             <tr class="bg-secondary text-white">
                                 <td colspan="4" class="text-right p-2"><h6>Subtotal: </h6></td>
-                                <td class="text-center p-2"><h5> {{$userCart->getSubtotal()}} </h5></td>
+                                <td class="text-center p-2"><h5> {{$userCart->getFormattedSubtotal()}} </h5></td>
                                 <td></td>
                             </tr>
                         </table>
@@ -62,6 +62,14 @@
                 @else
                     <br>
                     <p class="alert-info" colspan="5" style="font-size:110%; text-align: center">Ups! Your Shopping Cart is empty. Fill it with your favorite books! :)</p>
+                    <div class="text-center">
+                        <a class="btn btn-outline-primary justify-content-end" href="{{route('bookshelf')}}">
+                            Get more books
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-book" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                            </svg>
+                        </a>
+                    </div>
                 @endif
             </div>
         </div>
