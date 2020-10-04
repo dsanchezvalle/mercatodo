fe9af30d497a1e78495b3d466db85cee0e2f371d
David <dsanva2004@gmail.com>
2020-10-04 17:19:25 -0500

    feat: registering routes to manage orders and transactions.

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,13 @@ Route::get('/bookshelf', 'BookController@bookshelf')->middleware('verified')->na
 Route::get('/cart', 'OrderController@index')->middleware('verified')->name('cart.index');
 Route::get('/cart/checkout', 'OrderController@checkout')->middleware('verified')->name('cart.checkout');
 Route::post('/cart/{book}', 'OrderController@update')->middleware('verified')->name('cart.update');
-Route::get('/cart/payment', 'OrderController@payment')->middleware('verified')->name('cart.payment');
+Route::post('/payment', 'OrderController@payment')->middleware('verified')->name('cart.payment');
 Route::delete('/cart/{book}', 'OrderController@remove')->middleware('verified')->name('cart.remove');
+Route::put('/cart/{book}', 'OrderController@edit')->middleware('verified')->name('cart.edit');
+Route::get('/transaction/cancelled/{reference}', 'TransactionController@cancel')->name('transaction.cancel');
+Route::get('/transaction/{reference}', 'TransactionController@status')->name('transaction.status');
+Route::get('/transaction/result/{reference}', 'TransactionController@result')->name('transaction.result');
+Route::get('/orders', 'OrderController@list')->middleware('verified')->name('order.list');
 Route::get(
     '/uploads/{file}',
     function ($file) {
