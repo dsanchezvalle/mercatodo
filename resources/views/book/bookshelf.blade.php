3274060d4c8e7a56f94536ce28196b45b9ec9797
David <dsanva2004@gmail.com>
2020-10-04 17:21:03 -0500

    fix: adding Cart and Proceed to checkout buttons in alert message after adding new books to cart.

--- resources/views/book/bookshelf.blade.php
+++ resources/views/book/bookshelf.blade.php
@@ -19,11 +19,16 @@
             @if(session('message'))
                 <div class="alert alert-success">
                     {{session('message')}}
-                    <a class="btn btn-primary btn-sm" href="{{route('cart.index')}}">Cart</a>
-                    <a class="btn btn-success justify-content-end" href="{{ route('cart.checkout') }}">
-                        Proceed to checkout
+                    <a class="btn btn-primary btn-sm" href="{{route('cart.index')}}">
+                        Cart
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                        </svg>
+                    </a>
+                    <a class="btn btn-success btn-sm justify-content-end" href="{{ route('cart.checkout') }}">
+                        Proceed to checkout
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-wallet2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/>
                         </svg>
                     </a>
                 </div>
