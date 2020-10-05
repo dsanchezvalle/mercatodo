ce9a02ec8356739c497be46776880159e08fa7c4
David <dsanva2004@gmail.com>
2020-10-04 23:45:03 -0500

    refactor: creation of retry payment function. Comments deletion. Adding redirect in case of not successful payment.

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,38 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use App\Order;
 use App\Services\PlacetoPayServiceInterface;
 use App\Transaction;
-use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     public function cancel(int $reference, PlacetoPayServiceInterface $placetoPay)
     {
         Transaction::where('reference', $reference)->first()->update(['status' => 'REJECTED']);
-        //$transaction = Transaction::where('reference', $reference)->first();
-        //$response = $placetoPay->sessionQuery($transaction->request_id);
-        //dd($response);
-        //Trigger TRACKING PROCESS...
         return view('transaction.cancel', ['reference' => $reference]);
-        //dd('Your payment was rejected. Want to try again?');
     }
 
     public function status(int $reference, PlacetoPayServiceInterface $placetoPay)
     {
         $transaction = Transaction::where('reference', $reference)->first();
         $response = $placetoPay->sessionQuery($transaction->request_id);
+        $transaction->update(['status' => $response['status']['status']]);
+        return redirect()->route("transaction.result", $reference);
+    }
 
-//        if($response ['status']['status'] == 'APPROVED'){
-            $transactionStatus = $this->getPaymentStatus($response);  //Check REQUEST STATUS
-            $transaction->update(['status' => $transactionStatus]);
-            return redirect()->route("transaction.result", $reference);
-  //      }
-        //else{
-            //$response->
-        //}
+    public function retry(Request $request, $reference, PlacetoPayServiceInterface $placetoPay)
+    {
+        $transaction = Transaction::where('reference', $reference)->first();
+        $reference = $this->getReference();
+        $data = (array) json_decode($transaction->payment_data);
+        $payment = array_replace((array) $data['payment'], ['reference' => $reference]);
+        $data = array_replace( $data, [
+            'payment' => $payment,
+            'expiration' => date('c', strtotime('+30 minutes')),
+            'ipAddress' => $request->ip(),
+            'userAgent' => $request->header('User-Agent'),
+            'returnUrl' => 'http://mercatodo.test/transaction/' . $reference,
+            'cancelUrl' => 'http://mercatodo.test/transaction/cancelled/' . $reference,
+        ]);
 
+        $response = $placetoPay->payment($data);
+        if($response->isSuccessful()){
+            $transaction->order->update(['status' => 'closed']);
+
+            Transaction::create(
+                [
+                    'reference' => $reference,
+                    'order_id' => $transaction->order->id,
+                    'amount' => $transaction->order->getSubtotal(), //Check total or subtotal
+                    'request_id' => $response->requestId(),
+                    'status' => 'PENDING',
+                    'process_url' => $response->processUrl(),
+                    'payment_data' => json_encode($data),
+                ]
+            );
+
+            return redirect($response->processUrl());
+        }
+
+        return redirect(route('bookshelf'))
+            ->with('message', 'Unfortunately, your transaction could not be processed. Please try again later.');
     }
 
     public function result(int $reference)
@@ -41,8 +66,11 @@ class TransactionController extends Controller
         return view('transaction.result', ['reference' => $reference]);
     }
 
-    public function getPaymentStatus(array $response)
+    private function getReference()
     {
-        return  $response ['payment']['0']['status']['status'];
+        $timeStamp = Carbon::now()->format('YmdHis');
+        $userId = auth()->user()->id;
+
+        return $userId . $timeStamp;
     }
 }
