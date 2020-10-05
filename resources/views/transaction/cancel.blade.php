2c6eaecbd8ec1b117577ca83adf7f31d5850c329
David <dsanva2004@gmail.com>
2020-10-04 23:39:38 -0500

    fix: update rejected payment view.

--- resources/views/transaction/cancel.blade.php
+++ resources/views/transaction/cancel.blade.php
@@ -4,18 +4,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header h4"> Your Shopping Cart </div>
+            <div class="card text-center">
+                <div class="card-header">
+                </div>
                 <div class="card-body">
-                    <h3> Your payment has been rejected. :( </h3>
-                    <p> Reference: {{$reference}} </p>
-                    <br>
-                    <a href="{{ route('cart.checkout', ['reference' => $reference]) }}" class="btn btn-primary">Try again</a>
+                    <h2 class="card-title">Your payment has been rejected. :(</h2>
+                    <h1>
+                        <svg style="filter: invert(17%) sepia(51%) saturate(5437%) hue-rotate(355deg) brightness(91%) contrast(89%);" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                            <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                    </h1>
+                    <p class="card-text"> Your transaction with reference: {{$reference}} has been rejected. </p>
+                    <p><b>Don't worry, you can... </b></p>
+                    <a href="{{ route('transaction.retry', ['reference' => $reference]) }}" class="btn btn-primary">Retry</a>
                     <a href="{{ route('order.list') }}" class="btn btn-secondary">Check your orders</a>
-                </h3>
+                </div>
 
             </div>
-        </div>
     </div>
 
 
