2cb423549d6a82578a919db2c514f1718d8d0bd9
David <dsanva2004@gmail.com>
2020-11-24 15:35:36 -0500

    style: minor change welcome message.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header h4">Bienvenido a MercaTodo</div>
+                <div class="card-header h4">Welcome to MercaTodo</div>
 
                 <div class="card-body">
                     @if (session('status'))
