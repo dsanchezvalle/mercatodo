d5141b1bf813e61eb3289dccbc21d21bf0d13e9f
David <dsanva2004@gmail.com>
2020-11-24 15:40:58 -0500

    refactor: add validation rules for existent books, repeated ISBN in file and counters for created and updated books.

--- app/Imports/BooksImport.php
+++ app/Imports/BooksImport.php
@@ -3,55 +3,113 @@
 namespace App\Imports;
 
 use App\Book;
-use Illuminate\Database\Eloquent\Model;
-use Maatwebsite\Excel\Concerns\ToModel;
+use App\Exceptions\ImportValidationException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithValidation;
 
-class BooksImport implements ToModel, WithHeadingRow, WithValidation
+class BooksImport implements ToCollection, WithHeadingRow
 {
+    use Importable;
+    private $errors;
+    private $booksCreated;
+    private $booksUpdated;
+
     /**
-    * @param array $row
-    *
-    * @return Model|null
-    */
-    public function model(array $row)
+     * @param Collection $rows
+     * @return void
+     * @throws ImportValidationException
+     */
+    public function collection(Collection $rows)
     {
-        $existentBook = Book::find($row['id']);
-
-        if(!$existentBook){
-            return new Book([
-                'id' => $row['id'],
-                'isbn' => $row['isbn'],
-                'title' => $row['title'],
-                'author' => $row['author'],
-                'price' => $row['price'],
-                'stock' => $row['stock'],
-                'image_path' => $row['image_path']
-            ]);
+        $this->validateRows($rows);
+        foreach ($rows as $row) {
+            $existentBook = Book::find($row['id']);
+            if (!$existentBook) {
+                Book::create([
+                    'id' => $row['id'],
+                    'isbn' => $row['isbn'],
+                    'title' => $row['title'],
+                    'author' => $row['author'],
+                    'price' => $row['price'],
+                    'stock' => $row['stock'],
+                    'image_path' => $row['image_path']
+                ]);
+                ++$this->booksCreated;
+            } else {
+                $existentBook->update([
+                    'isbn' => $row['isbn'],
+                    'title' => $row['title'],
+                    'author' => $row['author'],
+                    'price' => $row['price'],
+                    'stock' => $row['stock'],
+                    'image_path' => $row['image_path'],
+                ]);
+                ++$this->booksUpdated;
+            }
         }
-
-        $existentBook->update([
-            'isbn' => $row['isbn'],
-            'title'=> $row['title'],
-            'author'=> $row['author'],
-            'price' => $row['price'],
-            'stock' => $row['stock'],
-            'image_path' => $row['image_path'],
-        ]);
+        //dd($this->booksUpdated, $this->booksCreated);
     }
 
-    public function rules(): array
+    /**
+     * @param Collection $rows
+     * @return void
+     * @throws ImportValidationException
+     */
+    private function validateRows(Collection $rows): void
     {
-        $id = $this->book->id ?? null;
-        return [
-            'isbn' => ['required', 'numeric', 'digits:13', "unique:books,isbn,$id"],
-            'title' => ['required', 'string', 'min: 2', 'max:100', 'regex:/^[a-zA-Z0-9áéíóúüñÑ\'\s\.]+$/'],
-            'author' => ['required', 'string', 'min: 2', 'max:80', 'regex:/^[a-zA-Z0-9áéíóúüñÑ\'\s\.]+$/'],
-            'price' => ['required', 'numeric', 'min:1', 'max:500000'],
-            'stock' => ['required', 'numeric', 'min:1', 'max:1000' ],
-            'image_path' => ['string', 'max:200']
-        ];
+        $this->verifyRepeatedIsbnInFile($rows);
+
+        foreach ($rows as $rowNumber => $row) {
+            try {
+                Validator::make($row->toArray(), [
+                    'id' => 'nullable',
+                    'isbn' => ['required', 'numeric', 'digits:13', "unique:books,isbn,{$row['id']}"],
+                    'title' => ['required', 'string', 'min: 2', 'max:100', 'regex:/^[a-zA-Z0-9áéíóúüñÑ\'\s\.]+$/'],
+                    'author' => ['required', 'string', 'min: 2', 'max:80', 'regex:/^[a-zA-Z0-9áéíóúüñÑ\'\s\.]+$/'],
+                    'price' => ['required', 'numeric', 'min:1', 'max:500000'],
+                    'stock' => ['required', 'numeric', 'min:1', 'max:1000'],
+                    'image_path' => ['string', 'max:200']
+                ])->validate();
+            } catch (ValidationException $e) {
+                $rowErrors = $e->errors();
+                array_walk_recursive($rowErrors, function ($error) use ($rowNumber) {
+                    $this->errors[] = sprintf('[row %s] %s', $rowNumber+2, $error);
+                });
+            }
+        }
+
+        if (!empty($this->errors)) {
+            throw new ImportValidationException($this->errors);
+        }
+    }
+
+    private function verifyRepeatedIsbnInFile(Collection $rows)
+    {
+        $isbnColumn = array_column($rows->toArray(), 'isbn');
+        foreach(array_count_values($isbnColumn) as $isbn => $occurrences){
+            if($occurrences>1){
+                $this->errors[] = sprintf(
+                    '[rows %s] These rows have repeated ISBN',
+                    implode(', ', array_map(function ($row) {
+                        return $row+2;
+                    }, array_keys($isbnColumn, $isbn)))
+                );
+            }
+        }
+    }
+
+    public function getBooksCreated()
+    {
+        return $this->booksCreated ?? '0';
+    }
+
+    public function getBooksUpdated()
+    {
+        return $this->booksUpdated ?? '0';
     }
 
 }
