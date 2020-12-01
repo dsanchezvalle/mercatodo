3021e19fd52213bb779e3f5c0b4e7608c34a796c
David <dsanva2004@gmail.com>
2020-12-01 16:38:03 -0500

    feature: create database migration and seeder for roles.

--- database/migrations/2020_11_26_162425_add_role_id_to_users_table.php
+++ database/migrations/2020_11_26_162425_add_role_id_to_users_table.php
@@ -16,6 +16,12 @@ class AddRoleIdToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->renameColumn('is_admin', 'role_id');
         });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->change();
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+
     }
 
     /**
@@ -26,7 +32,12 @@ class AddRoleIdToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_role_id_foreign');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
             $table->renameColumn('role_is', 'is_admin');
+            $table->string('is_admin')->default('0')->change();
         });
     }
 }
