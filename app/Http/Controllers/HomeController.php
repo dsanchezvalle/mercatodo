fc2f838585e7b98126e2bfa08de558f7497b38b6
David <dsanva2004@gmail.com>
2020-12-01 16:51:07 -0500

    feat: add permissions to editor to view the Home Dashboard.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if (Auth::user()->role_id == '1') {
+        if (Auth::user()->role_id == '1' || Auth::user()->role_id == '2') {
             return view('home');
         }
         return response()->redirectToRoute('bookshelf');
