7b2f46cd333929c283ecea6eaa842ed706281d88
David <dsanva2004@gmail.com>
2020-12-12 17:50:46 -0500

    style: update PHP DocBlock and remove unused code.

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\BookRequest;
 use App\Http\Resources\BookResource;
 use App\Http\Resources\BookResourceCollection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -15,9 +16,9 @@ class BookController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
         $books = Book::paginate(10);
         return (new BookResourceCollection($books))->response();
@@ -26,19 +27,12 @@ class BookController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param BookRequest $request
+     * @return JsonResponse
      */
-    public function store(BookRequest $request)
+    public function store(BookRequest $request) : JsonResponse
     {
-        /*if (!request()->file) {
-            return redirect('/books/create')
-                ->withInput($request->all())
-                ->withErrors(['file' => 'You need to add a book cover image.']);
-        }*/
-        //$imagePath = $this->get_image_path();
-        //$this->store_image($imagePath);
-        $imagePath = 'http://fakePath.com';
+
         $book = Book::create(
             [
                 'isbn' => $request->input('isbn'),
@@ -46,25 +40,27 @@ class BookController extends Controller
                 'author' => $request->input('author'),
                 'price' => $request->input('price'),
                 'stock' => $request->input('stock'),
-                'image_path' => $imagePath,
+                'image_path' => $request->input('image_path'),
                 'is_active' => true,
             ]
         );
         $idLastBookCreated = DB::table('books')->latest('id')->first()->id;
+
         Log::channel('single')
             ->notice("A new book with ID " . $idLastBookCreated . " has been created successfully by admin with ID: " . $request->user()->id);
         Log::channel('slack')
             ->notice("A new book has been created successfully. Find it at:  http://mercatodo.test/books/" . $idLastBookCreated);
-        return (new BookResource($book))->response()->setStatusCode(Response::HTTP_CREATED);
+
+        return (new BookResource($book))->response();
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
+     * @param Book $book
+     * @return JsonResponse
      */
-    public function show(Book $book)
+    public function show(Book $book): JsonResponse
     {
         return (new BookResource($book))->response();
     }
@@ -72,20 +68,12 @@ class BookController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
+     * @param BookRequest $request
+     * @param Book $book
+     * @return JsonResponse
      */
-    public function update(BookRequest $request, Book $book)
+    public function update(BookRequest $request, Book $book): JsonResponse
     {
-        /*if (request()->file) {
-            $imagePath = $this->get_image_path();
-            $this->store_image($imagePath);
-            unlink(storage_path() . '/app' . $book->image_path);
-        } else {
-            $imagePath = $book->image_path;
-        }*/
-        $imagePath = 'http://fakePath.com';
         $book->update(
             [
                 'isbn' => $request->input('isbn'),
@@ -93,7 +81,7 @@ class BookController extends Controller
                 'author' => $request->input('author'),
                 'price' => $request->input('price'),
                 'stock' => $request->input('stock'),
-                'image_path' => $imagePath,
+                'image_path' => $request->input('image_path'),
                 'is_active' => $request->is_active ? true : false,
             ]
         );
@@ -102,16 +90,5 @@ class BookController extends Controller
             ->notice("The book with ID " . $book->id . " has been updated successfully by admin with ID: " . $request->user()->id);
 
         return (new BookResource($book))->response();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Book $book)
-    {
-        //
     }
 }
