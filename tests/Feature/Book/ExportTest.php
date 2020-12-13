a3ee79f83f28a1cd8a1b9a683b4418cbc1c60cdc
David <dsanva2004@gmail.com>
2020-12-12 19:46:35 -0500

    feat: create import and export tests.

---
+++ tests/Feature/Book/ExportTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Book;
+
+use App\Book;
+use App\Exports\BooksExport;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class ExportTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAdminCanExportBooks()
+    {
+        Excel::fake();
+        $user = factory(User::class)->create(['role_id' => '1']);
+        $books = factory(Book::class, 10)->create();
+
+        $this->actingAs($user)->get(route('books.export'));
+
+        Excel::assertDownloaded('books.xlsx', function (BooksExport $export) use ($books) {
+            return $export->collection()->count() === $books->count();
+        });
+    }
+
+    public function testEditorCanExportBooks()
+    {
+        Excel::fake();
+        $user = factory(User::class)->create(['role_id' => '2']);
+        $books = factory(Book::class, 10)->create();
+
+        $this->actingAs($user)->get(route('books.export'));
+
+        Excel::assertDownloaded('books.xlsx', function (BooksExport $export) use ($books) {
+            return $export->collection()->count() === $books->count();
+        });
+    }
+
+    public function testBuyerCanNotExportBooks()
+    {
+        $user = factory(User::class)->create(['role_id' => '3']);
+
+        $response = $this->actingAs($user)->get(route('books.export'));
+
+        $response->assertForbidden();
+    }
+
+    public function testNonAuthenticatedUserCanNotExportBooks()
+    {
+        $this->get(route('books.export'))->assertRedirect(route('verification.notice'));
+    }
+
+
+}
