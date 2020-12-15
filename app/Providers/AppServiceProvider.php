ff782b25a159fd190ee61259ed124f0c8a1470f2
David <dsanva2004@gmail.com>
2020-12-15 06:02:58 -0500

    feat: include currency in config/app.php and add COP and USD cases in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Decorators\CurrencyCOPDecorator;
+use App\Decorators\CurrencyUSDDecorator;
+use App\Decorators\PriceFormatter;
+use App\Decorators\PriceFormatterContract;
 use App\Services\{PlacetoPayService, PlacetoPayServiceInterface};
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
@@ -22,5 +26,24 @@ class AppServiceProvider extends ServiceProvider
                 return new Client();
             }
         );
+    }
+
+    /**
+     *
+     */
+    public function boot()
+    {
+        $this->app->bind(PriceFormatterContract::class, function(){
+            $formatter = new PriceFormatter();
+
+            switch(config('app.currency')){
+                case 'USD':
+                    return New CurrencyUSDDecorator($formatter);
+                case 'COP':
+                    return New CurrencyCOPDecorator($formatter);
+            }
+
+            return $formatter;
+        });
     }
 }
